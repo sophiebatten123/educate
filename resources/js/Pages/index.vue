3404b7d90f9b5a803d010bbe2ae9f2b2bdc2e17c
sophiebatten123 <sophie_batten123@hotmail.co.uk>
2023-08-17 17:03:00 +0100

    tailwind has been added to the project for styling

--- resources/js/Pages/index.vue
+++ resources/js/Pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        this is index page SOPHIE WAS HERE || SO AND PAV :)
+    <div class="font-bold underline text-3xl">
+        <p class="font-bold underline text-red-200">hello</p>
     </div>
     <div>{{message}}</div>
 </template>
