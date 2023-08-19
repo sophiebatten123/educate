0cc31953e325f9ee580f8bafad30ae2aa30031ae
Sophie Batten <sophie.batten@classroomsecrets.co.uk>
2023-08-19 22:41:35 +0100

    drawing, square, circle and img has been added to the interactive whiteboard

--- resources/js/Pages/index.vue
+++ resources/js/Pages/index.vue
@@ -3,11 +3,20 @@
         <p class="font-bold underline text-blue-900 mb-4">Educate Whiteboard</p>
     </div>
     <div class="flex">
-        <button class="bg-orange-200 h-20">
-            <p>Pencil</p>
+        <button class="rounded-full w-12 h-12 border-2 border-black h-20" @click="togglePencil" :class="pencil ? 'bg-yellow-400' : ''">
+            <p><font-awesome-icon :icon="['fas', 'pencil']" /></p>
         </button>
-        <button class="bg-purple-200 h-20">
-            <p>Rubber</p>
+        <!-- <button class="rounded-full border-2 border-black w-12 h-12 h-20 mx-4" @click="toggleEraser" :class="eraser ? 'bg-yellow-400' : ''">
+            <p><font-awesome-icon :icon="['fas', 'eraser']" /></p>
+        </button> -->
+        <button class="rounded-full w-12 h-12 border-2 border-black h-20 mx-4" @click="toggleSquare" :class="square ? 'bg-yellow-400' : ''">
+            <p><font-awesome-icon :icon="['fas', 'square']" /></p>
+        </button>
+        <button class="rounded-full w-12 h-12 border-2 border-black h-20" @click="toggleCircle" :class="circle ? 'bg-yellow-400' : ''">
+            <p><font-awesome-icon :icon="['fas', 'circle']" /></p>
+        </button>
+        <button class="rounded-full w-12 h-12 border-2 border-black h-20 mx-4" @click="toggleImage" :class="image ? 'bg-yellow-400' : ''">
+            <p><font-awesome-icon :icon="['fas', 'image']" /></p>
         </button>
     </div>
     <div class="flex">
@@ -15,37 +24,137 @@
              <div class="bg-red-200" style="height: 300px;">Tutor Camera</div>
              <div class="bg-green-200" style="height: 300px;">Student Camera</div>
         </div>
-        <canvas ref="canvas" class="bg-white border-2 border-black"></canvas>
+        <canvas ref="canvasRef" :width="canvasWidth" :height="canvasHeight" class="border-2 border-black"></canvas>
     </div>
 </template>
 
 <script>
 import { fabric } from 'fabric';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+
+// Add the solid (fas) icons to the library
+library.add(fas);
 
 export default {
+  data() {
+    return {
+      pencil: true,
+      eraser: false,
+      square: false,
+      circle: false,
+      image: false,
+      canvasWidth: 1000,
+      canvasHeight: 1000,
+      canvas: null,
+      myImage: "https://cdn-icons-png.flaticon.com/256/5264/5264272.png"
+    }
+  },
+  components: {
+    FontAwesomeIcon
+  },
   mounted() {
     this.initializeCanvas();
   },
   methods: {
     initializeCanvas() {
-      this.canvas = new fabric.Canvas(this.$refs.canvas, {
-        width: 1150,
-        height: 600
+      this.canvas = new fabric.Canvas(this.$refs.canvasRef, {
+        isDrawingMode: true,
       });
-
-      // Set up drawing functionality
-      this.canvas.isDrawingMode = true;
-      this.canvas.freeDrawingBrush.width = 5;
-
-      // Register the path:created event
-      this.canvas.on('path:created', (event) => {
-        console.log(event.path);
-        const path = event.path;
-        this.canvas.add(path);
-        this.canvas.renderAll();
-      });
-      
+      window.addEventListener('keydown', this.handleKeyDown);
     },
+    togglePencil() {
+      this.pencil = !this.pencil;
+      this.eraser = false;
+      this.square = false;
+      this.canvas.isDrawingMode = this.pencil;
+    },
+    toggleImage() {
+      this.image = !this.image;
+      this.pencil = false;
+      this.eraser = false;
+      this.square = false;
+      this.canvas.isDrawingMode = false;
+      new fabric.Image.fromURL(this.myImage, (_img)=>{
+        const _daisy = _img.set({left:0, top:0, width: _img.width, height: _img.height})
+        this.canvas.add(_daisy)
+      })
+    },
+    // toggleEraser() {
+    //   this.eraser = !this.eraser;
+    //   this.pencil = false;
+    //   this.square = false;
+    //   this.canvas.isDrawingMode = false;
+    // },
+    toggleSquare() {
+      if (!this.square) {
+        this.createSquare();
+        this.square = true;
+
+        setTimeout(() => {
+          this.square = false;
+        }, 1000); // Toggle back after 1 second
+      }
+      this.pencil = false;
+      this.eraser = false;
+      this.circle = false;
+    },
+    createSquare() {
+      this.canvas.isDrawingMode = false;
+
+      const rect = new fabric.Rect({
+        top: 50,
+        left: 50,
+        height: 100,
+        width: 100,
+        selectable: true, // Make the rectangle selectable
+      });
+
+      this.canvas.add(rect);
+      this.canvas.setActiveObject(rect);
+    },
+    toggleCircle() {
+      if (!this.circle) {
+        this.createCircle();
+        this.circle = true;
+
+        setTimeout(() => {
+          this.circle = false;
+        }, 1000); // Toggle back after 1 second
+      }
+      this.pencil = false;
+      this.eraser = false;
+      this.square = false;
+    },
+    createCircle() {
+      this.canvas.isDrawingMode = false;
+
+      const circle = new fabric.Circle({
+        top: 50,
+        left: 50,
+        radius: 40,
+        fill: "green",
+        selectable: true, // Make the circle selectable
+      });
+
+      this.canvas.add(circle);
+      this.canvas.setActiveObject(circle);
+    },
+    handleKeyDown(event) {
+      if (event.keyCode === 8) { // Backspace key
+        const activeObject = this.canvas.getActiveObject();
+        if (activeObject) {
+          this.canvas.remove(activeObject);
+          this.canvas.discardActiveObject();
+          this.canvas.renderAll();
+        }
+      }
+    },
+  },
+  beforeDestroy() {
+    // Clean up the keydown event listener when the component is destroyed
+    window.removeEventListener('keydown', this.handleKeyDown);
   },
 };
 </script>
